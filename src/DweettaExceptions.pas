fcbe2a71d587393cff07bda8b2c2cdad255a680f
gcarreno <gcarreno@localhost>
2009-05-01 17:49:14 +0000

    Completed Exceptions with the JSON returned object's data.

--- src/DweettaExceptions.pas
+++ src/DweettaExceptions.pas
@@ -21,24 +21,31 @@ type
 
   EDweettaTransportError  = class(Exception)
     private
-      FHttpStatusCode: Integer;
+      FHTTPStatusCode: Integer;
+      FHTTPMessage: String;
+      FOriginalRequest: String;
+      FJSONMessage: String;
     public
-      constructor Create(HttpErrorCode: Integer); overload;
-      constructor Create(HttpErrorCode: Integer; originalReq: String; errorStr: string); overload;
+      constructor Create(aHTTPErrorCode: Integer); overload;
+      constructor Create(aHTTPErrorCode: Integer; aOriginalReq: String; aErrorStr: String); overload;
+      constructor Create(aHTTPErrorCode: Integer; aOriginalReq: String; aErrorStr: String; aJSONMessage: String); overload;
 
-      property HttpStatusCode: Integer read FHttpStatusCode;
+      property HTTPStatusCode: Integer read FHTTPStatusCode;
+      property HTTPMessage: String read FHTTPMessage;
+      property OriginalRequest: String read FOriginalRequest;
+      property JSONMessage: String read FJSONMessage;
   end;
 
 implementation
 
 { EDweettaTransportError }
 
-constructor EDweettaTransportError.Create ( HttpErrorCode: Integer ) ;
+constructor EDweettaTransportError.Create (aHTTPErrorCode: Integer) ;
 var
   strError: String;
 begin
-  FHttpStatusCode := HttpErrorCode;
-  case HttpErrorCode of
+  FHTTPStatusCode := aHTTPErrorCode;
+  case aHTTPErrorCode of
     400: strError := '400 - Bad Request: You may have exceeded the rate limit';
     401: strError := '401 - Not Authorized: either you need to provide authentication credentials, or the credentials provided aren not valid.';
     403: strError := '403 - Forbidden';
@@ -46,20 +53,29 @@ begin
     500: strError := '500 - Internal Server Error';
     502: strError := '502 - Bad Gateway, Dweetta may be down or being upgraded';
     503: strError := '503 - Service Unavailable, The Dweetta Servers are overloaded and need more steam.';
-    else strError := 'HTTP Status Code: ' + IntToStr(HttpErrorCode) + ' - Oops - Something else went wrong';
+    else strError := 'HTTP Status Code: ' + IntToStr(aHTTPErrorCode) + ' - Oops - Something else went wrong';
   end;
 
   inherited Create(strError);
 end;
 
-constructor EDweettaTransportError.Create ( HttpErrorCode: Integer;
-  originalReq: String; errorStr: string ) ;
+constructor EDweettaTransportError.Create (aHTTPErrorCode: Integer;
+  aOriginalReq: String; aErrorStr: string ) ;
 begin
-  FHttpStatusCode := HttpErrorCode;
-  // We haven't used originalReq here but it's a copy of the request
-  // eg.. <request>/direct_messages/destroy/456.xml</request>
+  FHTTPStatusCode := aHTTPErrorCode;
+  FOriginalRequest := aOriginalReq;
+  FHTTPMessage := aErrorStr;
+  inherited Create(aErrorStr);
+end;
 
-  inherited Create(errorStr);
+constructor EDweettaTransportError.Create(aHttpErrorCode: Integer;
+  aOriginalReq: String; aErrorStr: String; aJSONMessage: String);
+begin
+  FHTTPStatusCode := aHTTPErrorCode;
+  FOriginalRequest := aOriginalReq;
+  FHTTPMessage := aErrorStr;
+  FJSONMessage := aJSONMessage;
+  inherited Create(aErrorStr);
 end;
 
 end.
